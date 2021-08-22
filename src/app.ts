93a550567bc2ee2e326649c6b04a015bcf8af2e8
s.grujic <s.grujic@emakina.rs>
2021-08-22 18:41:00 +0200

    PCR: split string against regexp pattern

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,26 @@
 import * as fs from 'fs'
 
 function app() {
+    // Transform text file into string
     fs.readFile('./public/claims.txt', 'utf8', (err, data) => {
         if (err) throw err
         const text = data
-        console.log(text)
+        splitString(text)
+        console.log(splitString(text).length)
     })
     console.log('Execute!')
+}
+
+//Split string against pattern
+//Returns new array of strings
+function splitString(text: string): string[] {
+    if (text === '') {
+        throw new Error('String is empty')
+    }
+
+    const arrOfClaims = text.split(/\n\d{1,}\./)
+
+    return arrOfClaims
 }
 
 app()
