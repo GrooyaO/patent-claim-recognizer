2b308525508b2201191daef1360775c28024775a
s.grujic <s.grujic@emakina.rs>
2021-08-22 18:30:04 +0200

    PCR: read file, added fs module

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('Execute!')
+import * as fs from 'fs'
+
+function app() {
+    fs.readFile('./public/claims.txt', 'utf8', (err, data) => {
+        if (err) throw err
+        const text = data
+        console.log(text)
+    })
+    console.log('Execute!')
+}
+
+app()
